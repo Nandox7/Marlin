0e1469df5c0580fa3fa165fc298c3eba7e99e36d
Nando <nandox7@gmail.com>
2015-07-05 02:08:07 +0100

    Fix Configuration.h for Hephestos and correct sensor Z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -402,8 +402,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 200
-#define Y_MAX_POS 200
+#define X_MAX_POS 215
+#define Y_MAX_POS 210
 #define Z_MAX_POS 180
 
 //===========================================================================
@@ -495,8 +495,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // Offsets to the probe relative to the extruder tip (Hotend - Probe)
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER -25     // Probe on: -left  +right
-  #define Y_PROBE_OFFSET_FROM_EXTRUDER -29     // Probe on: -front +behind
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -12.35  // -below (always!)
+  #define Y_PROBE_OFFSET_FROM_EXTRUDER -27     // Probe on: -front +behind
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -4.50  // -below (always!)
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
